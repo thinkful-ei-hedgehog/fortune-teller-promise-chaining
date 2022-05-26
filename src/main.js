776940f8c356da757e2f5276fb2138ee97b40f51
Terra Taylor <terra.taylor@gmail.com>
2022-05-26 13:33:59 -0500

    async await

--- src/main.js
+++ src/main.js
@@ -11,14 +11,20 @@ function getFortune(question) {
   return ask(question).catch(err=>`There was an error: ${err}`);
 }
 
-function fullSession(question) {
+async function fullSession(question) {
     //welcome text
-   return welcome().then(welcome=>
+    let welcomeMessage = await welcome();
+    let fortuneArr = await getFortune(question);
+    let goodbyeMess = await goodbye();
+    let arr = [welcomeMessage].concat(fortuneArr);
+    arr.push(goodbyeMess);
+    return arr
+   /*return welcome().then(welcome=>
      getFortune(question)//get q & a
       .then(fortuneArr=>[welcome].concat(fortuneArr)) //combined with welcome
       .then(fortuneSoFar=> goodbye()//say goodbye
       .then(goodbyeMess=> [...fortuneSoFar,goodbyeMess])) //combine goodbye with array so far
-    )
+    )*/
    
     
     
